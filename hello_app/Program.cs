2d678f23ed485a138e81dd88e71818e6ba78edf6
Michelle <vantablanta@gmail.com>
2022-08-12 09:55:55 +0300

    Number methods explored

--- hello_app/Program.cs
+++ hello_app/Program.cs
@@ -9,34 +9,58 @@ namespace HelloApp
 
             //DATA TYPES
             /*
-            string name = "George";
-            int age = 35;
-            char grade = 'A';
-            double weight = 3.6;
-            bool isSmoker = false;
+                string name = "George";
+                int age = 35;
+                char grade = 'A';
+                double weight = 3.6;
+                bool isSmoker = false;
 
-            Console.WriteLine("There was once a man named " + name);
-            Console.WriteLine("He was a " + age + " years old");
-            Console.WriteLine("He Scored an " + grade);
-            Console.WriteLine("He weighed " + weight + " kgs" );
-            Console.WriteLine(isSmoker ? "He smokes " : "He doesnt smoke");
+                Console.WriteLine("There was once a man named " + name);
+                Console.WriteLine("He was a " + age + " years old");
+                Console.WriteLine("He Scored an " + grade);
+                Console.WriteLine("He weighed " + weight + " kgs" );
+                Console.WriteLine(isSmoker ? "He smokes " : "He doesnt smoke");
             */
 
 
             //STRING METHODS 
+            /*
+                Console.WriteLine("Girrafe Academy \n is the best says \" Michelle \" ");
+                Console.WriteLine("Michelle " + "Njeri");
+
+                string phrase = "C# is cool";
+                Console.WriteLine(phrase);
+                Console.WriteLine(phrase.Length);
+
+                //METHODS
+                Console.WriteLine(phrase.ToUpper());
+                Console.WriteLine(phrase.Contains("cool"));  // returns boolean 
+                Console.WriteLine(phrase[0]);
+                Console.WriteLine(phrase.Substring(5,2));   
+             */
+
+
+            // NUMBER METHODS 
+            /*
+                int num = 6;
+                Console.WriteLine(5 / 2);
+                Console.WriteLine(num);
+                num++;
+                Console.WriteLine(num);
+
+                //METHODS
+                Console.WriteLine(Math.Abs(-40));
+                Console.WriteLine(Math.Pow(8, 2));
+                Console.WriteLine(Math.Sqrt(64));
+                Console.WriteLine(Math.Max(50, 454)); //takes only 2 arguments
+                Console.WriteLine(Math.Round(4.99));
+             */
+
+            //GETTING USER INPUT 
             /**/
-            Console.WriteLine("Girrafe Academy \n is the best says \" Michelle \" ");
-            Console.WriteLine("Michelle " + "Njeri");
 
-            string phrase = "C# is cool";
-            Console.WriteLine(phrase);
-            Console.WriteLine(phrase.Length);
 
-            //METHODS
-            Console.WriteLine(phrase.ToUpper());
-            Console.WriteLine(phrase.Contains("cool"));  // returns boolean 
-            Console.WriteLine(phrase[0]);
-            Console.WriteLine();   
+
 
 
 
